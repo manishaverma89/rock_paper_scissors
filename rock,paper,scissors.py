5f162776b9bc27ee9d90ce892ec01f188f3da17a
manisha <manisha.verma23@gmail.com>
2022-05-15 18:55:38 +0530

    rock,paper,scissors game includes randomisation, lists, if-elif loops…

--- rock,paper,scissors.py
+++ rock,paper,scissors.py
@@ -26,7 +26,7 @@ scissors = '''
 ---.__(___)
 '''
 
-#Write your code below this line 👇
+
 game_images = [rock, paper, scissors] 
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper and 2 for Scissors\n"))
 
